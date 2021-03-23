d6d0f7d3f21a8b84dcf814517b8042cb239f96db
Paul Gilmartin <paulgilmartin@Pauls-MacBook-Pro.local>
2021-03-23 13:41:27 +0100

    Create separate dockerfiles for envs

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -125,7 +125,10 @@ USE_TZ = True
 # Prod seems to be using STATIC_URL even when debug is off?
 STATIC_URL = '/static/'
 if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # location of static files in development
+    STATICFILES_DIRS = ['/personal_website/static']
+else:
+    STATICFILES_DIRS = []
+# location of static files in development
 # When in prod, Django compiles all static files into this dir via collectstatic command (using the
 # STATICFILES_FINDERS env var, which is set for us)
 
@@ -133,7 +136,7 @@ if DEBUG:
 # To get this to work in develop, we need to comment this
 # out. Find a better way - probably by making STATIC_ROOT
 # something like var/www/personal_website/, not just personal_website/static
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/var/www/personal_website/static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
